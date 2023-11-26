d3c63fc886d54d340e915ddeb075e42e2047dea0
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-11-26 08:53:35 -0300

    feat: connect to postgres

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const userController = require('./controllers/users')
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,16 @@ app.use(cors({ origin: true }));
 
 const PROJECT_ID = process.env.CHAT_ENGINE_PROJECT_ID
 const PRIVATE_KEY = process.env.CHAT_ENGINE_PRIVATE_KEY
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', userController.getUsers)
+app.get('/users/:id', userController.getUserById)
+app.post('/users', userController.createUser)
+app.put('/users/:id', userController.updateUser)
+app.delete('/users/:id', userController.deleteUser)
 
 app.post("/signup", async (req, res) => {
   const { username, secret, email, first_name, last_name } = req.body;
@@ -49,7 +60,11 @@ app.post("/login", async (req, res) => {
   }
 });
 
+const port = process.env.PORT || 3001
+
 app.listen({
   host: '0.0.0.0',
-  port: process.env.PORT || 3001,
+  port,
+}, () => {
+  console.log(`App running on port ${port}.`)
 });
